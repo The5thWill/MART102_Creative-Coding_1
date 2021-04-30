d7d1b14b90bdd027b82ca9c8b9d8fb5a6f179ca1
The5thWill <54751772+The5thWill@users.noreply.github.com>
2021-04-30 16:48:21 -0400

    Final

--- Homework_Final/gallery4.js
+++ Homework_Final/gallery4.js
@@ -1,9 +1,8 @@
 function setup()
 function preload() {
-  img = loadImage("/images/mountain_sun.jpg");
-}
-{
-  img.loadPixels(mountain_sun.jpg);
+  img = loadImage("/images/field.jpg");}
+  {
+  img.loadPixels(field.jpg);
   for (x = 0; x < img.width; x = x + 5) {
    for (y = 0; y < img.height; y = y + 5) {
       index = (floor(x) + floor(y) * img.width) * 4;
@@ -14,6 +13,6 @@ function preload() {
       strokeWeight(9 * Math.random())
       stroke(red, blue, green, alpha/(5 * Math.random()))
       line(x + Math.random() * 40,y ,x + 50,y + 50 * Math.random())
-}
-}
-}
+  }
+  }
+  }
