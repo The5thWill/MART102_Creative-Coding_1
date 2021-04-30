5c315704fe4acf3ef7ec8ecf34c6c91465395863
The5thWill <54751772+The5thWill@users.noreply.github.com>
2021-04-30 05:06:49 -0400

    Final

---
+++ Homework_Final/Portrait.js
@@ -0,0 +1,55 @@
+function setup()
+{
+  img.loadPixels(mountain_sun.jpg);
+  for (x = 0; x < img.width; x = x + 5) {
+   for (y = 0; y < img.height; y = y + 5) {
+      index = (floor(x) + floor(y) * img.width) * 4;
+      red = img.pixels[index]
+      blue = img.pixels[index + 1]
+      green = img.pixels[index + 2]
+      alpha = img.pixels[index + 3]
+      strokeWeight(9 * Math.random())
+      stroke(red, blue, green, alpha/(5 * Math.random()))
+      line(x + Math.random() * 40,y ,x + 50,y + 50 * Math.random())
+}
+}
+img.loadPixels(snow_lake.jpg);
+for (x = 0; x < img.width; x = x + 5) {
+ for (y = 0; y < img.height; y = y + 5) {
+    index = (floor(x) + floor(y) * img.width) * 4;
+    red = img.pixels[index]
+    blue = img.pixels[index + 1]
+    green = img.pixels[index + 2]
+    alpha = img.pixels[index + 3]
+    strokeWeight(9 * Math.random())
+    stroke(red, blue, green, alpha/(5 * Math.random()))
+    line(x + Math.random() * 40,y ,x + 50,y + 50 * Math.random())
+}
+}
+img.loadPixels(sun_lake.jpg);
+for (x = 0; x < img.width; x = x + 5) {
+ for (y = 0; y < img.height; y = y + 5) {
+    index = (floor(x) + floor(y) * img.width) * 4;
+    red = img.pixels[index]
+    blue = img.pixels[index + 1]
+    green = img.pixels[index + 2]
+    alpha = img.pixels[index + 3]
+    strokeWeight(9 * Math.random())
+    stroke(red, blue, green, alpha/(5 * Math.random()))
+    line(x + Math.random() * 40,y ,x + 50,y + 50 * Math.random())
+}
+}
+img.loadPixels(field.jpg);
+for (x = 0; x < img.width; x = x + 5) {
+ for (y = 0; y < img.height; y = y + 5) {
+    index = (floor(x) + floor(y) * img.width) * 4;
+    red = img.pixels[index]
+    blue = img.pixels[index + 1]
+    green = img.pixels[index + 2]
+    alpha = img.pixels[index + 3]
+    strokeWeight(9 * Math.random())
+    stroke(red, blue, green, alpha/(5 * Math.random()))
+    line(x + Math.random() * 40,y ,x + 50,y + 50 * Math.random())
+}
+}
+}
